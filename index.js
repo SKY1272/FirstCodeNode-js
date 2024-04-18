078ab2d3cad1ee81cc8ea443c7d9e94ac9c85e2f
SKY1272 <MY_NAME@example.com>
2024-04-18 17:45:03 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('hello world')
+// 1
+const arrow=(a,b)=>{
+  return a*b;
+}
+console.log(arrow(5,6))
+//  2
+const student={
+   name:'shivpujan',
+   age:24,
+   greet(){
+    console.log('hi i am'+" "+this.name+"."+'my age is  '+this.age)
+   }
+}
+student.greet()
